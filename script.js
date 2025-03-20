4cd409d0e58bd362f505fdbbf14b3d86819cc1d3
shebin <mohammedshebinc92@gmail.com>
2025-03-20 10:46:08 +0530

    jwt

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const jwt=require("jsonwebtoken")
+
+
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+  try {
+    const token=jwt.sign(payload,secret,{expiresIn:"1h"})
+    return token
+  } catch (error) {
+    throw new Error('Error generating token: ' + error.message);
+  }
+
 };
 
-module.exports = encrypt;
+const verifyToken=(token,secret)=>{
+  try {
+    const decoded=jwt.verify(token,secret)
+    return decoded
+  } catch (error) {
+    return { valid: false, expired: error.name === 'TokenExpiredError' };
+  }
+}
+
+module.exports = {encrypt,verifyToken};
